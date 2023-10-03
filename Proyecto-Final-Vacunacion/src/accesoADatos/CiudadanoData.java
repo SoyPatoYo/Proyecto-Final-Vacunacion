e3f1ca06e8ae279dbdf5efb04418e9ad7474b0b9
SoyPatoYo <matiaspato3@gmail.com>
2023-10-03 20:03:15 -0300

    Update CiudadanoData.java

--- Proyecto-Final-Vacunacion/src/accesoADatos/CiudadanoData.java
+++ Proyecto-Final-Vacunacion/src/accesoADatos/CiudadanoData.java
@@ -5,10 +5,50 @@
  */
 package accesoADatos;
 
+import entidades.Ciudadano;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Matias
  */
 public class CiudadanoData {
+
+    private Connection conexion = null;
+
+    public CiudadanoData() {
+        conexion = Conexion.buscarConexion();
+    }
+
+    public void guardarCiudadano(Ciudadano persona) {
+        String sql = "INSERT INTO ciudadano VALUES (?,?,?,?,?,?,?,?,?)";
+
+        try {
+            PreparedStatement ps = conexion.prepareStatement(sql);
+            ps.setInt(1, persona.getDni());
+            ps.setString(2, persona.getNombre());
+            ps.setString(3, persona.getApellido());
+            ps.setString(4, persona.getZona());
+            ps.setString(5, persona.getEmail());
+            ps.setInt(6, persona.getCelular());
+            ps.setString(7, persona.getPatologia());
+            ps.setString(8, persona.getAmbitoTrabajo());
+            ps.setBoolean(9, persona.isCovid());
+
+            int exito = ps.executeUpdate();
+            if (exito == 1) {
+                JOptionPane.showMessageDialog(null, "Ciudadano guardado.");
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error: " + ex.getMessage());
+        }
+
+    }
     
+    
+
 }
