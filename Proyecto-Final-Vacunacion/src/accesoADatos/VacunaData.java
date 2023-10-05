644a18bd58f903c9d72bd0ee689052b6c9c8adfd
SoyPatoYo <matiaspato3@gmail.com>
2023-10-05 19:41:26 -0300

    cosas

--- Proyecto-Final-Vacunacion/src/accesoADatos/VacunaData.java
+++ Proyecto-Final-Vacunacion/src/accesoADatos/VacunaData.java
@@ -6,6 +6,8 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
@@ -49,11 +51,46 @@ public class VacunaData {
             ps.setInt(1, idDosis);
             ResultSet rs = ps.executeQuery();
             if (rs.next()) {
+                vacuna = new Vacuna();
+                vacuna.setIdDosis(rs.getInt("idDosis"));
+                vacuna.setMarca(rs.getString("marca"));
+                vacuna.setLaboratorio(rs.getString("laboratorio"));
+                vacuna.setMedida(rs.getDouble("medida"));
+                vacuna.setVencimiento(rs.getDate("vencimiento").toLocalDate());
+                vacuna.setAntigeno(rs.getString("antigeno"));
+                vacuna.setColocada(rs.getBoolean("colocada"));
             }
         } catch (SQLException ex) {
-            Logger.getLogger(VacunaData.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, "Error: " + ex.getMessage());
         }
         return vacuna;
+    }
+
+    public List<Vacuna> buscarTodasVacunas() {
+        List<Vacuna> vacunas = new ArrayList<>();
+        String sql = "SELECT * FROM vacuna";
+
+        try {
+            PreparedStatement ps = conexion.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Vacuna vacuna = new Vacuna();
+                vacuna.setIdDosis(rs.getInt("idDosis"));
+                vacuna.setMarca(rs.getString("marca"));
+                vacuna.setLaboratorio(rs.getString("laboratorio"));
+                vacuna.setMedida(rs.getDouble("medida"));
+                vacuna.setVencimiento(rs.getDate("vencimiento").toLocalDate());
+                vacuna.setAntigeno(rs.getString("antigeno"));
+                vacuna.setColocada(rs.getBoolean("colocada"));
+
+                vacunas.add(vacuna);
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error: " + ex.getMessage());
+        }
+
+        return vacunas;
     }
 
 }
