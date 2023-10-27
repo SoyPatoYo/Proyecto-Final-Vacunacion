a7e99424f516364e2544a0501d0c21c98f5375f6
LucasNLopez <lucaslopezlnlaba@gmail.com>
2023-10-27 19:50:13 -0300

    sad

--- Proyecto-Final-Vacunacion/src/vistas/ListarCitas.java
+++ Proyecto-Final-Vacunacion/src/vistas/ListarCitas.java
@@ -152,8 +152,9 @@ public class ListarCitas extends javax.swing.JPanel {
         int filaSeleccionada = jTable1.getSelectedRow();
         if (filaSeleccionada >= 0) {
             int dniPersona = Integer.parseInt(jTable1.getValueAt(filaSeleccionada, 0).toString());
+            
             int idCita = -1;
-            List<Cita> listaCitas = citaD.listarCitasPorPersona(idPersona);
+            List<Cita> listaCitas = citaD.listarCitas();
             for (Cita cita : listaCitas) {
                 if (cita.getPersona().getDni()== dniPersona) {
                     idCita = cita.getCodigo();
